0ab2d4469e09ab5ba2b2cea2924cda03bbd7482e
unknown <steven@eddies.me.uk>
2022-06-04 02:28:53 +0100

    Added initial iteration of switching between flags, and fixed colours.

--- Flags/__init__.py
+++ Flags/__init__.py
@@ -1,11 +1,13 @@
 from tidal import *
 from app import App
+from buttons import Buttons
 
 class FlagsApp(App):
 	
 	def __init__(self):
 		super().__init__()
-		self.current_index = 1
+		self.mybuttons = Buttons()
+		self.current_index = 0
 		self.flags = [
 			self.draw_rainbow,
 			self.draw_ace
@@ -15,22 +17,30 @@ class FlagsApp(App):
 		super().on_activate()
 		self.draw_current()
 	
+	def on_start(self):
+		super().on_start()
+		self.mybuttons.on_press(JOY_RIGHT, self.next_flag)
+	
+	def next_flag(self):
+		self.current_index = (self.current_index + 1) % len(self.flags)
+		self.draw_current()
+	
 	def draw_current(self):
 		self.flags[self.current_index]()
 	
 	def draw_rainbow(self):
 		display.fill(color565(228, 3, 3))
-		display.fill_rect(22, 0, 23, 240, color565(255, 40, 0))
+		display.fill_rect(22, 0, 23, 240, color565(255, 140, 0))
 		display.fill_rect(45, 0, 22, 240, color565(255, 237, 0))
-		display.fill_rect(67, 0, 23, 240, color565(0, 28, 38))
+		display.fill_rect(67, 0, 23, 240, color565(0, 128, 38))
 		display.fill_rect(90, 0, 22, 240, color565(0, 77, 255))
-		display.fill_rect(2, 0, 23, 240, color565(7, 7,35))
+		display.fill_rect(112, 0, 23, 240, color565(117, 7, 135))
 	
 	def draw_ace(self):
 		display.fill(color565(0, 0, 0))
-		display.fill_rect(34, 0, 34, 240, color565(	64, 65, 64))
+		display.fill_rect(34, 0, 34, 240, color565(164, 165, 164))
 		display.fill_rect(68, 0, 34, 240, color565(255, 255, 255))
-		display.fill_rect(102, 0, 33, 240, color565(29, 0, 29))
+		display.fill_rect(102, 0, 33, 240, color565(129, 0, 129))
 
 
 main = FlagsApp
