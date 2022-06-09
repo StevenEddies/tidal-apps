b5cf2783e94ba79bc2ab29703fc24ec30d1a9762
unknown <steven@eddies.me.uk>
2022-06-09 19:30:35 +0100

    Added dummy plugin.

---
+++ buildSrc/src/main/groovy/greetingplugin.groovy
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.gradle.api.*;
+
+class GreetingPlugin implements Plugin<Project> {
+    void apply(Project project) {
+        project.task('hello') {
+            doLast {
+                println 'Hello from the GreetingPlugin'
+            }
+        }
+    }
+}
