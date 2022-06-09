9cf7d6a9cc01d2b13710a757c23f3eef81e01761
Steven Eddies <steven@eddies.me.uk>
2022-06-09 19:59:16 +0100

    Refactored Gradle plugin structure.

---
+++ tidal-app-plugin/src/main/java/uk/me/eddies/gradleplugins/tidalapp/TidalAppPlugin.java
@@ -0,0 +1,13 @@
+package uk.me.eddies.gradleplugins.tidalapp;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class TidalAppPlugin implements Plugin<Project> {
+
+	@Override
+	public void apply(Project arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
